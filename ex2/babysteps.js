4d31458a39d3138eb622effec1ef912835b5a57c
somabc <somabc@gmail.com>
2017-03-26 17:39:25 +0100

    comments on babysteps

--- ex2/babysteps.js
+++ ex2/babysteps.js
@@ -1,5 +1,9 @@
+# initialise
 var result = 0;
+# loop starting at 2nd argument until end of all arguments
 for (var i = 2; i < process.argv.length; i++) {
+# add arguments together while converting strings to numbers
     result += Number(process.argv[i]);
 }
+# print the result
 console.log(result);
